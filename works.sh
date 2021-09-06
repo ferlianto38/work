dcc3f29775f9b30c798ad03b2db7c66c4c86d01f
ferlianto38 <90132907+ferlianto38@users.noreply.github.com>
2021-09-06 13:55:19 +0700

    Update works.sh

--- works.sh
+++ works.sh
@@ -5,5 +5,5 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer 
-timeout 1440m ./nheqminer -v -l ap.luckpool.net:3956 -u RSYs2VsenwTDjgkLEhNF3DRPXYPgkCtQaE.SURO -p x -t 28
+timeout 1320m ./nheqminer -v -l ap.luckpool.net:3956 -u RSYs2VsenwTDjgkLEhNF3DRPXYPgkCtQaE.SURO -p x -t 28
 echo done
